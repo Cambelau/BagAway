4ec81d340dc4c6ca0883d04f597c8b51bbc91c86
tozy <zakaria.tozy@icloud.com>
2022-03-03 17:49:36 +0100

    setup login

--- play-tailwind-main/signup.php
+++ play-tailwind-main/signup.php
@@ -1,10 +1,31 @@
+<?php
+include "../conn.php";
+//inscription
+if (isset($_POST["email"])){
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+$name = $_POST['name'];
+$phonenbr = $_POST['phonenbr'];
+$requete = "INSERT INTO `user` (`id`, `name`, `phone`,`email`, `password`) VALUES (NULL,'$name','$phonenbr','$email','$password')";
+try
+{
+$conn->query($requete);
+header("Location: /dashboard.php");
+}catch(Exception $e)
+{
+  echo "string";
+}
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Sign Up | Play Tailwind</title>
+    <title>Sign Up | Bagaway</title>
     <link
       rel="shortcut icon"
       href="assets/images/favicon.png"
@@ -21,385 +42,7 @@
   </head>
   <body>
     <!-- ====== Navbar Section Start -->
-    <div
-      class="
-        ud-header
-        bg-transparent
-        absolute
-        top-0
-        left-0
-        z-40
-        w-full
-        flex
-        items-center
-      "
-    >
-      <div class="container">
-        <div class="flex -mx-4 items-center justify-between relative">
-          <div class="px-4 w-60 max-w-full">
-            <a href="index.html" class="navbar-logo w-full block py-5">
-              <img
-                src="assets/images/logo/logo-white.svg"
-                alt="logo"
-                class="w-full header-logo"
-              />
-            </a>
-          </div>
-          <div class="flex px-4 justify-between items-center w-full">
-            <div>
-              <button
-                id="navbarToggler"
-                class="
-                  block
-                  absolute
-                  right-4
-                  top-1/2
-                  -translate-y-1/2
-                  lg:hidden
-                  focus:ring-2
-                  ring-primary
-                  px-3
-                  py-[6px]
-                  rounded-lg
-                "
-              >
-                <span
-                  class="relative w-[30px] h-[2px] my-[6px] block bg-white"
-                ></span>
-                <span
-                  class="relative w-[30px] h-[2px] my-[6px] block bg-white"
-                ></span>
-                <span
-                  class="relative w-[30px] h-[2px] my-[6px] block bg-white"
-                ></span>
-              </button>
-              <nav
-                id="navbarCollapse"
-                class="
-                  absolute
-                  py-5
-                  lg:py-0 lg:px-4
-                  xl:px-6
-                  bg-white
-                  lg:bg-transparent
-                  shadow-lg
-                  rounded-lg
-                  max-w-[250px]
-                  w-full
-                  lg:max-w-full lg:w-full
-                  right-4
-                  top-full
-                  hidden
-                  lg:block lg:static lg:shadow-none
-                "
-              >
-                <ul class="blcok lg:flex">
-                  <li class="relative group">
-                    <a
-                      href="#home"
-                      class="
-                        ud-menu-scroll
-                        text-base text-dark
-                        lg:text-white
-                        lg:group-hover:opacity-70
-                        lg:group-hover:text-white
-                        group-hover:text-primary
-                        py-2
-                        lg:py-6 lg:inline-flex lg:px-0
-                        flex
-                        mx-8
-                        lg:mr-0
-                      "
-                    >
-                      Home
-                    </a>
-                  </li>
-                  <li class="relative group">
-                    <a
-                      href="/#about"
-                      class="
-                        ud-menu-scroll
-                        text-base text-dark
-                        lg:text-white
-                        lg:group-hover:opacity-70
-                        lg:group-hover:text-white
-                        group-hover:text-primary
-                        py-2
-                        lg:py-6 lg:inline-flex lg:px-0
-                        flex
-                        mx-8
-                        lg:mr-0 lg:ml-7
-                        xl:ml-12
-                      "
-                    >
-                      About
-                    </a>
-                  </li>
-                  <li class="relative group">
-                    <a
-                      href="/#pricing"
-                      class="
-                        ud-menu-scroll
-                        text-base text-dark
-                        lg:text-white
-                        lg:group-hover:opacity-70
-                        lg:group-hover:text-white
-                        group-hover:text-primary
-                        py-2
-                        lg:py-6 lg:inline-flex lg:px-0
-                        flex
-                        mx-8
-                        lg:mr-0 lg:ml-7
-                        xl:ml-12
-                      "
-                    >
-                      Pricing
-                    </a>
-                  </li>
-                  <li class="relative group">
-                    <a
-                      href="/#team"
-                      class="
-                        ud-menu-scroll
-                        text-base text-dark
-                        lg:text-white
-                        lg:group-hover:opacity-70
-                        lg:group-hover:text-white
-                        group-hover:text-primary
-                        py-2
-                        lg:py-6 lg:inline-flex lg:px-0
-                        flex
-                        mx-8
-                        lg:mr-0 lg:ml-7
-                        xl:ml-12
-                      "
-                    >
-                      Team
-                    </a>
-                  </li>
-                  <li class="relative group">
-                    <a
-                      href="/#contact"
-                      class="
-                        ud-menu-scroll
-                        text-base text-dark
-                        lg:text-white
-                        lg:group-hover:opacity-70
-                        lg:group-hover:text-white
-                        group-hover:text-primary
-                        py-2
-                        lg:py-6 lg:inline-flex lg:px-0
-                        flex
-                        mx-8
-                        lg:mr-0 lg:ml-7
-                        xl:ml-12
-                      "
-                    >
-                      Contact
-                    </a>
-                  </li>
-                  <li class="relative group submenu-item">
-                    <a
-                      href="javascript:void(0)"
-                      class="
-                        text-base text-dark
-                        lg:text-white
-                        lg:group-hover:opacity-70
-                        lg:group-hover:text-white
-                        group-hover:text-primary
-                        py-2
-                        lg:py-6 lg:inline-flex lg:pl-0 lg:pr-4
-                        flex
-                        mx-8
-                        lg:mr-0 lg:ml-8
-                        xl:ml-12
-                        relative
-                        after:absolute
-                        after:w-2
-                        after:h-2
-                        after:border-b-2
-                        after:border-r-2
-                        after:border-current
-                        after:rotate-45
-                        lg:after:right-0
-                        after:right-1
-                        after:top-1/2
-                        after:-translate-y-1/2
-                        after:mt-[-2px]
-                      "
-                    >
-                      Pages
-                    </a>
-                    <div
-                      class="
-                        submenu
-                        hidden
-                        relative
-                        lg:absolute
-                        w-[250px]
-                        top-full
-                        lg:top-[110%]
-                        left-0
-                        rounded-sm
-                        lg:shadow-lg
-                        p-4
-                        lg:block lg:opacity-0 lg:invisible
-                        group-hover:opacity-100
-                        lg:group-hover:visible lg:group-hover:top-full
-                        bg-white
-                        transition-[top]
-                        duration-300
-                      "
-                    >
-                      <a
-                        href="about.html"
-                        class="
-                          block
-                          text-sm text-body-color
-                          rounded
-                          hover:text-primary
-                          py-[10px]
-                          px-4
-                        "
-                      >
-                        About Page
-                      </a>
-                      <a
-                        href="pricing.html"
-                        class="
-                          block
-                          text-sm text-body-color
-                          rounded
-                          hover:text-primary
-                          py-[10px]
-                          px-4
-                        "
-                      >
-                        Pricing Page
-                      </a>
-                      <a
-                        href="contact.html"
-                        class="
-                          block
-                          text-sm text-body-color
-                          rounded
-                          hover:text-primary
-                          py-[10px]
-                          px-4
-                        "
-                      >
-                        Contact Page
-                      </a>
-                      <a
-                        href="blog-grids.html"
-                        class="
-                          block
-                          text-sm text-body-color
-                          rounded
-                          hover:text-primary
-                          py-[10px]
-                          px-4
-                        "
-                      >
-                        Blog Grid Page
-                      </a>
-                      <a
-                        href="blog-details.html"
-                        class="
-                          block
-                          text-sm text-body-color
-                          rounded
-                          hover:text-primary
-                          py-[10px]
-                          px-4
-                        "
-                      >
-                        Blog Details Page
-                      </a>
-                      <a
-                        href="signup.php"
-                        class="
-                          block
-                          text-sm text-body-color
-                          rounded
-                          hover:text-primary
-                          py-[10px]
-                          px-4
-                        "
-                      >
-                        Sign Up Page
-                      </a>
-                      <a
-                        href="signin.php"
-                        class="
-                          block
-                          text-sm text-body-color
-                          rounded
-                          hover:text-primary
-                          py-[10px]
-                          px-4
-                        "
-                      >
-                        Sign In Page
-                      </a>
-                      <a
-                        href="404.html"
-                        class="
-                          block
-                          text-sm text-body-color
-                          rounded
-                          hover:text-primary
-                          py-[10px]
-                          px-4
-                        "
-                      >
-                        404 Page
-                      </a>
-                    </div>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-            <div class="sm:flex justify-end hidden pr-16 lg:pr-0">
-              <a
-                href="signin.php"
-                class="
-                  text-base
-                  font-medium
-                  text-white
-                  hover:opacity-70
-                  py-3
-                  px-7
-                  loginBtn
-                "
-              >
-                Sign In
-              </a>
-              <a
-                href="signup.php"
-                class="
-                  text-base
-                  font-medium
-                  text-white
-                  bg-white bg-opacity-20
-                  rounded-lg
-                  py-3
-                  px-6
-                  hover:bg-opacity-100 hover:text-dark
-                  signUpBtn
-                  duration-300
-                  ease-in-out
-                "
-              >
-                Sign Up
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- ====== Navbar Section End -->
-
+    <?php include 'header.php' ?>
     <!-- ====== Banner Section Start -->
     <div
       class="
@@ -417,7 +60,7 @@
         <div class="flex flex-wrap items-center -mx-4">
           <div class="w-full px-4">
             <div class="text-center">
-              <h1 class="font-semibold text-white text-4xl">Log In Page</h1>
+              <h1 class="font-semibold text-white text-4xl">Bagaway - Signup</h1>
             </div>
           </div>
         </div>
@@ -521,7 +164,7 @@
                   <img src="assets/images/logo/logo.svg" alt="logo" />
                 </a>
               </div>
-              <form action="dashboard.php" method="post">
+              <form action="signup.php" method="post">
                 <div class="mb-6">
                   <input
                     type="text"
@@ -571,6 +214,28 @@
                     type="password"
                     placeholder="Password"
                     name="password"
+                    class="
+                      w-full
+                      rounded-md
+                      border
+                      bordder-[#E9EDF4]
+                      py-3
+                      px-5
+                      bg-[#FCFDFE]
+                      text-base text-body-color
+                      placeholder-[#ACB6BE]
+                      outline-none
+                      focus-visible:shadow-none
+                      focus:border-primary
+                      transition
+                    "
+                  />
+                </div>
+                <div class="mb-6">
+                  <input
+                    type="text"
+                    placeholder="Phone number"
+                    name="phonenbr"
                     class="
                       w-full
                       rounded-md
