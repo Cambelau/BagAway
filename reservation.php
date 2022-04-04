9e23b5f6dad49eaa2abd069ddf14ecaf83ace340
Cambelau <sajotmatthieu@gmail.com>
2022-04-04 12:44:05 +0200

    front update

--- reservation.php
+++ reservation.php
@@ -1,12 +1,51 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
+<html lang="en" class="scroll-smooth">
   <head>
-    <meta charset="utf-8">
-    <title>Reservation d'un camion</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>
+      BagAway - Main Page
+    </title>
+    <link
+      rel="shortcut icon"
+      href="assets/images/favicon.png"
+      type="image/x-icon"
+    />
+    <link rel="stylesheet" href="assets/css/animate.css" />
+    <link rel="stylesheet" href="assets/css/tailwind.css" />
+    <link href="https://cdn.lineicons.com/3.0/lineicons.css" rel="stylesheet">
+    <!-- ==== WOW JS ==== -->
+    <script src="assets/js/wow.min.js"></script>
+    <script>
+      new WOW().init();
+    </script>
   </head>
   <body>
-    <h1>Reservation du camion</h1>
-    <form action="reservation-1.php" method="post">
+  <?php include 'header.php'?>
+    <div
+      id="home"
+      class="relative pt-[120px] md:pt-[130px] lg:pt-[160px] bg-primary"
+    >
+    <div class="container">
+      <div class="flex flex-wrap items-center -mx-4 min-h-[780px]">
+
+      <div class="w-full px-4">
+      <div
+              class="
+                hero-content
+                text-center
+                max-w-[780px]
+                
+                mx-auto
+                wow
+                fadeInUp
+              "
+              data-wow-delay=".2s"
+            >
+        <form action="reservation-1.php" method="post">
+      Reservation du camion
+      <br>
         <label for="nombre de bagage">Bagages : </label>
         <select class="" name="nbrbag">
           <option value="1">1</option>
@@ -22,5 +61,87 @@
         <br>
        <button type="submit" name="button">Confirmer la reservation</button>
     </form>
+    </div>
+    </div>
+    </div>
+          </div>
+
+    <!-- ====== Back To Top Start -->
+    <a
+      href="javascript:void(0)"
+      class="
+        hidden
+        items-center
+        justify-center
+        bg-primary
+        text-white
+        w-10
+        h-10
+        rounded-md
+        fixed
+        bottom-8
+        right-8
+        left-auto
+        z-[999]
+        hover:bg-dark
+        back-to-top
+        shadow-md
+        transition
+        duration-300
+        ease-in-out
+      "
+    >
+      <span
+        class="w-3 h-3 border-t border-l border-white rotate-45 mt-[6px]"
+      ></span>
+    </a>
+    <!-- ====== Back To Top End -->
+
+    <!-- ====== All Scripts -->
+
+    <script src="assets/js/main.js"></script>
+    <script>
+      // ==== for menu scroll
+      const pageLink = document.querySelectorAll(".ud-menu-scroll");
+
+      pageLink.forEach((elem) => {
+        elem.addEventListener("click", (e) => {
+          e.preventDefault();
+          document.querySelector(elem.getAttribute("href")).scrollIntoView({
+            behavior: "smooth",
+            offsetTop: 1 - 60,
+          });
+        });
+      });
+
+      // section menu active
+      function onScroll(event) {
+        const sections = document.querySelectorAll(".ud-menu-scroll");
+        const scrollPos =
+          window.pageYOffset ||
+          document.documentElement.scrollTop ||
+          document.body.scrollTop;
+
+        for (let i = 0; i < sections.length; i++) {
+          const currLink = sections[i];
+          const val = currLink.getAttribute("href");
+          const refElement = document.querySelector(val);
+          const scrollTopMinus = scrollPos + 73;
+          if (
+            refElement.offsetTop <= scrollTopMinus &&
+            refElement.offsetTop + refElement.offsetHeight > scrollTopMinus
+          ) {
+            document
+              .querySelector(".ud-menu-scroll")
+              .classList.remove("active");
+            currLink.classList.add("active");
+          } else {
+            currLink.classList.remove("active");
+          }
+        }
+      }
+
+      window.document.addEventListener("scroll", onScroll);
+    </script>
   </body>
 </html>
