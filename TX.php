e22f7f6b619b4cc3310f55c554e88b8136144a29
tozy <zakaria.tozy@icloud.com>
2022-04-05 15:16:33 +0200

    ett

--- TX.php
+++ TX.php
@@ -7,11 +7,11 @@ The ESP will send a link like the one above but with more than just b1. It will 
 //We loop through and grab variables from the received the URL
 foreach($_REQUEST as $key => $value)   //Save the received value to the hey variable. Save each cahracter after the "&"
 {
-    
+
     if($key =="id")
     {
        $id_casier = $value;
-    }	
+    }
     if($key =="lat"){
 		$lat = $value;
 	}
@@ -21,7 +21,7 @@ foreach($_REQUEST as $key => $value)   //Save the received value to the hey vari
 	if($key =="sta"){
 		$sta = $value;
 	}
-	
+
 }//End of foreach
 
 
@@ -29,34 +29,37 @@ include("conn.php"); 	//We include the database_connect.php which has the data f
 
 
 // Check  the connection
-/*if (mysqli_connect_errno()) {
+if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
 //Now we update the values in database
-		
+
 		//udpdate gps
-		//mysqli_query($conn,"INSERT INTO ESPtable2 (SENT_NUMBER_1,SENT_NUMBER_2,SENT_NUMBER_3,SENT_NUMBER_4,RECEIVED_BOOL1,TEXT_1) VALUES ('$sent_nr_1', '$sent_nr_2', '$sent_nr_3', '$sent_nr_4',0,'Nom')");
-		       
-	         $result = mysqli_query($conn,$sql = "SELECT 'Ouverture' FROM 'casier' WHERE 'id_casier' = $id_casier)";
-		
 
-//Loop through the table and filter out data for this unit id equal to the one taht we've received. 
-if($row = mysqli_fetch_array($result)) {
 
-	
-		$statut = $row['Ouverture'];	
-		
+	         $req = "SELECT * FROM `casier` WHERE `id_casier` = $id_casier;";
+             $res = $conn->query($req);
+             $row = $res->fetch_array();
+
+
+
+//Loop through the table and filter out data for this unit id equal to the one taht we've received.
+if(isset($row[0])) {
+
+
+		$statut = $row[4];
+		if($statut==1){
+		    $sql = "UPDATE `casier` SET `Ouverture`= 0 WHERE `id_casier`= $id_casier;";
+		    $res = $conn->query($sql);
+
+		}
+
 	echo "transmission ok  $statut ";
-}// End of the if*/
-
-//echo "transmission ok  $statut ";
+}// End of the if
+else
+{
+    echo"erreur";
+}
+//echo "transmission ok  1 ";
 ?>
-
-
-
-
-
-
-
-
